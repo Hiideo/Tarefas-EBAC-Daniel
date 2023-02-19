e3fbe1ff466ef2ce79fc3a2799d890cb488aa7e2
Daniel <dahiwa93@gmail.com>
2023-02-19 12:06:13 -0300

    Tarefa do 22º Modulo

---
+++ mod22 tarefa_Streams/SepStream/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+?
+src/Pessoa.java,0\a\0ae21033df9e12143637b1a0506c6207b8c8c964
+H
+src/SeparandoStream.java,8\c\8cf142b6984b5f1800d0a0a0f1a385d8920e0ddc
---
+++ mod22 tarefa_Streams/SepStream/src/Pessoa.java
@@ -0,0 +1,23 @@
+
+public class Pessoa {
+    String nome;
+    String genero;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+}
